2bc0fddb86d13b5efc61281e7a79c50e77daf5f2
Zen-Vora <zenvora11@gmail.com>
2025-01-10 15:45:59 -0500

    Update affirmation_adventures.java

--- core/src/main/java/com/badlogic/affirmation_adventures/affirmation_adventures.java
+++ core/src/main/java/com/badlogic/affirmation_adventures/affirmation_adventures.java
@@ -1,11 +1,22 @@
 package com.badlogic.affirmation_adventures;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class affirmation_adventures extends Game {
+
+    public SpriteBatch batch;
+    public BitmapFont font;
+    public FitViewport viewport;
     @Override
     public void create() {
-        setScreen(new MainMenuScreen());
+        setScreen(new MainMenuScreen(this));
+
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        viewport = new FitViewport(8, 5);
     }
 }

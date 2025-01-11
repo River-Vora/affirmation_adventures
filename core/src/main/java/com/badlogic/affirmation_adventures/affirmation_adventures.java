1766f36d52d258be520b5feb41537a5d70c09b9e
Zen-Vora <zenvora11@gmail.com>
2025-01-11 17:28:42 -0500

    Fix NullPointerException

--- core/src/main/java/com/badlogic/affirmation_adventures/affirmation_adventures.java
+++ core/src/main/java/com/badlogic/affirmation_adventures/affirmation_adventures.java
@@ -5,21 +5,21 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class affirmation_adventures extends Game {
 
     public SpriteBatch batch;
     public BitmapFont font;
-    public FitViewport viewport;
+    public Viewport viewport;
 
     @Override
     public void create() {
-        setScreen(new MainMenuScreen(this));
 
         batch = new SpriteBatch();
         font = new BitmapFont();
-        viewport = new FitViewport(8, 5);
+        viewport = new FitViewport(800, 500);
 
         font.setUseIntegerPositions(false);
         font.getData().setScale(viewport.getWorldHeight() / Gdx.graphics.getHeight());
@@ -29,6 +29,7 @@ public class affirmation_adventures extends Game {
 
     @Override
     public void resize(int width, int height) {
+        viewport.update(width, height, true);
         super.resize(width, height);
     }
 

ac61f518d1871020fa8c4e5ec4a24a75a4917f0d
Zen-Vora <zenvora11@gmail.com>
2025-01-26 17:08:06 -0500

    Update game screen,

--- core/src/main/java/com/badlogic/affirmation_adventures/GameScreen.java
+++ core/src/main/java/com/badlogic/affirmation_adventures/GameScreen.java
@@ -1,15 +1,26 @@
 package com.badlogic.affirmation_adventures;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.utils.ScreenUtils;
 
-public class GameScreen implements Screen {
+public class GameScreen implements Screen{
     final affirmation_adventures game;
 
-    Texture playerTexture;
-    Texture backgroundTexture;
-    Sprite playerSprite;
+    public Texture playerTexture;
+    public Sprite playerSprite;
+    public OrthogonalTiledMapRenderer mapRenderer;
+    public TiledMap map;
+    public OrthographicCamera camera;
+
     int torch1X = 903;
     int torch1Y = 137;
     int torch2X = 712;
@@ -30,12 +41,17 @@ public class GameScreen implements Screen {
     public GameScreen(final affirmation_adventures game) {
         this.game = game;
 
-        backgroundTexture = new Texture("Dungeon.png");
         playerTexture = new Texture("player.png");
 
         playerSprite = new Sprite(playerTexture);
+        playerSprite.setSize(5, 5);
 
+        map = new TmxMapLoader().load("Dungeon.tmx");
+        mapRenderer = new OrthogonalTiledMapRenderer(map, 2 / 16f);
 
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 30, 20);
+        camera.update();
     }
 
     @Override
@@ -52,6 +68,20 @@ public class GameScreen implements Screen {
     }
 
     private void draw() {
+        ScreenUtils.clear(Color.BLACK);
+        game.viewport.apply();
+        game.batch.setProjectionMatrix(game.viewport.getCamera().combined);
+
+        camera.position.x = playerSprite.getX();
+        camera.position.y = playerSprite.getY();
+        camera.update();
+
+        mapRenderer.setView(camera);
+        mapRenderer.render();
+
+        game.batch.begin();
+        playerSprite.draw(game.batch);
+        game.batch.end();
 
     }
 
@@ -60,12 +90,31 @@ public class GameScreen implements Screen {
     }
 
     private void input() {
+        float speed = 100f;
+        float delta = Gdx.graphics.getDeltaTime();
 
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            playerSprite.translateX(speed * delta);
+        }
+
+        else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            playerSprite.translateX(-speed * delta);
+        }
+
+        else if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            playerSprite.translateY(speed * delta);
+        }
+
+        else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            playerSprite.translateY(-speed * delta);
+        }
     }
+
+
 
     @Override
     public void resize(int width, int height) {
-
+        game.viewport.update(width, height, true);
     }
 
     @Override
@@ -85,6 +134,9 @@ public class GameScreen implements Screen {
 
     @Override
     public void dispose() {
+        playerTexture.dispose();
+        mapRenderer.dispose();
+        map.dispose();
 
     }
 }

385aabdedf5458ff85eab033b49392ef85649834
Zen Vora <zenvora11@gmail.com>
2025-02-09 19:00:34 -0500

    This commit adds javadoc comments to the OptionsScreen.
    Signed-off-by: Zen-Vora <zenvora11@gmail.com>

--- core/src/main/java/com/badlogic/affirmation_adventures/OptionsScreen.java
+++ core/src/main/java/com/badlogic/affirmation_adventures/OptionsScreen.java
@@ -2,43 +2,81 @@ package com.badlogic.affirmation_adventures;
 
 import com.badlogic.gdx.Screen;
 
+/**
+ * Represents the options screen where the player can change settings.
+ */
+
 public class OptionsScreen implements Screen {
     final affirmation_adventures game;
+
+    /**
+     * Constructs a new OptionsScreen.
+     * @param game the main game instance
+     */
 
     public OptionsScreen(final affirmation_adventures game) {
         this.game = game;
     }
+
+    /**
+     * Prepares the screen for display.
+     */
 
     @Override
     public void show() {
 
     }
 
+    /**
+     * Renders the screen.
+     * @param delta the time in seconds since the last render
+     */
+
     @Override
     public void render(float delta) {
 
     }
+
+    /**
+     * Resizes the screen.
+     * @param width the new width of the screen
+     * @param height the new height of the screen
+     */
 
     @Override
     public void resize(int width, int height) {
 
     }
 
+    /**
+     * Pauses the screen.
+     */
+
     @Override
     public void pause() {
 
     }
 
+    /**
+     * Resumes the screen after pausing.
+     */
     @Override
     public void resume() {
 
     }
+
+    /**
+     * Hides the screen.
+     */
 
     @Override
     public void hide() {
 
     }
 
+    /**
+     * Disposes of the screen and its assets when they are no longer needed.
+     */
     @Override
     public void dispose() {
 

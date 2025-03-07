1ab6185712a1ac48c453b856c411631e3eb68447
River Vora <vorariver@gmail.com>
2025-03-07 15:54:38 -0500

    Create GameOverScreen.java class.

---
+++ core/src/main/java/com/badlogic/affirmation_adventures/GameOverScreen.java
@@ -0,0 +1,51 @@
+package com.badlogic.affirmation_adventures;
+
+import com.badlogic.gdx.Screen;
+
+public class GameOverScreen implements Screen {
+
+    @Override
+    public void show() {
+
+    }
+
+    /**
+     * Called when the screen should render itself.
+     *
+     * @param delta The time in seconds since the last render.
+     */
+    @Override
+    public void render(float delta) {
+
+    }
+
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+
+    @Override
+    public void pause() {
+
+    }
+
+
+    @Override
+    public void resume() {
+
+    }
+
+
+    @Override
+    public void hide() {
+
+    }
+
+
+    @Override
+    public void dispose() {
+
+    }
+}

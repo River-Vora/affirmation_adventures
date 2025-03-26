e32eab593952429fcb22dffc26d16492d96ef4b0
River Vora <vorariver@gmail.com>
2025-03-26 18:08:48 -0400

    Add license headers to configuration-cache-report.html and overview-tree.html

--- lwjgl3/build/docs/javadoc/type-search-index.js
+++ lwjgl3/build/docs/javadoc/type-search-index.js
@@ -1,1 +1,17 @@
+/**
+ * Copyright 2025 River Vora
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.badlogic.affirmation_adventures.lwjgl3","l":"Lwjgl3Launcher"},{"p":"com.badlogic.affirmation_adventures.lwjgl3","l":"StartupHelper"}];updateSearchResults();

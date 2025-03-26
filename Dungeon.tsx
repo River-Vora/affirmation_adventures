164f7cc1defd4859be167f4f95993f4b977acdc2
River Vora <vorariver@gmail.com>
2025-03-26 18:08:12 -0400

    Delete ios module and add license headers to files

--- Dungeon.tsx
+++ Dungeon.tsx
@@ -1,4 +1,20 @@
 <?xml version="1.0" encoding="UTF-8"?>
+/**
+ * Copyright 2025 River Vora
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 <tileset version="1.10" tiledversion="1.11.1" name="Dungeon" tilewidth="16" tileheight="16" tilecount="7168" columns="64">
  <image source="lwjgl3/build/resources/main/Dungeon.png" trans="ff00ff" width="1024" height="1792"/>
  <tile id="213">
